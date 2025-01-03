30dac0bd39cf314963b977bfe1bcb481c2498034
Bramhananda K L <149486769+KLBramhananda@users.noreply.github.com>
2025-01-03 23:15:28 +0530

    Update server.js

--- FormBot-Builder-APP/backend/server.js
+++ FormBot-Builder-APP/backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "",
+      "https://form-bot-builder-app-frontrunning.vercel.app/",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
