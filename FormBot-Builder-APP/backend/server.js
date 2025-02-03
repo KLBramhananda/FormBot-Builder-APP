f1f1f16b3ffcfb4e3c9dfa348bfe8e5c3f7b74ef
KLBramhananda <bramhanandakl@gmail.com>
2025-02-03 18:56:43 +0530

    changes done

--- FormBot-Builder-APP/backend/server.js
+++ FormBot-Builder-APP/backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: `${process.env.REACT_APP_FRONTEND_URL}`,
+  origin: `http://localhost:3000`,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type'],
   credentials: true
