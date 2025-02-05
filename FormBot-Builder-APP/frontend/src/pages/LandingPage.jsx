29a7708407ddc950787f63ee2070d918a4c4e0f2
KLBramhananda <bramhanandakl@gmail.com>
2025-02-05 13:04:44 +0530

    changes updated

--- FormBot-Builder-APP/frontend/src/pages/LandingPage.jsx
+++ FormBot-Builder-APP/frontend/src/pages/LandingPage.jsx
@@ -1,12 +1,17 @@
 import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import "./LandingPage.css";
 
 const LandingPage = () => {
+  const navigate = useNavigate();
+
   const fetchData = async () => {
     try {
-      const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/endpoint`);
+      const response = await fetch(
+        `${process.env.REACT_APP_BACKEND_URL}/api/endpoint`
+      );
       const data = await response.json();
       console.log("Fetched Data:", data);
     } catch (error) {
@@ -17,6 +22,10 @@ const LandingPage = () => {
   useEffect(() => {
     fetchData();
   }, []);
+
+  const handleCreateFormbot = () => {
+    navigate("/signup");
+  };
 
   return (
     <div className="landing-page">
@@ -30,17 +39,32 @@ const LandingPage = () => {
               <h1>visually</h1>
             </span>
             <p>
-              Typebot gives you powerful blocks to create unique chat experiences. Embed them <br />
-              anywhere on your web/mobile apps and start collecting results like magic.
+              Typebot gives you powerful blocks to create unique chat
+              experiences. Embed them <br />
+              anywhere on your web/mobile apps and start collecting results like
+              magic.
             </p>
-            <button className="create-formbot-btn">Create a Formbot for free</button>
+            <button
+              className="create-formbot-btn"
+              onClick={handleCreateFormbot}
+            >
+              Create a Formbot for free
+            </button>
           </div>
           <img src="./assets/logos/half-circle-logo.png" alt="" />
         </div>
 
         <div className="image-container">
-          <img src="./assets/images/yellow.png" alt="Blue Background" className="left-image" />
-          <img src="./assets/images/blue.png" alt="Yellow Background" className="right-image" />
+          <img
+            src="./assets/images/yellow.png"
+            alt="Blue Background"
+            className="left-image"
+          />
+          <img
+            src="./assets/images/blue.png"
+            alt="Yellow Background"
+            className="right-image"
+          />
           <img
             src="./assets/images/landingpage-image.png"
             alt="Formbot preview"
